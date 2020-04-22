9eadbf1877042a94ca6f960c3adf8a892e0deafe
Roger Braunstein <roger@partlyhuman.com>
2020-04-21 20:10:07 -0700

    Convert phantomjs to puppeteer! Add pixel scaling option

--- batch-capture.js
+++ batch-capture.js
@@ -4,9 +4,8 @@ const path = require('path');
 const fs = require('fs');
 const shell = require('shelljs');
 const program = require('commander');
+const puppeteer = require('puppeteer');
 const fileUrl = require('file-url');
-const phantom = require('phantom');
-const phantomPath = require('phantomjs-prebuilt').path;
 const imagemin = require('imagemin');
 const imagemin_pngquant = require('imagemin-pngquant');
 const imagemin_mozjpeg = require('imagemin-mozjpeg');
@@ -24,25 +23,27 @@ function sleep(timeout) {
 }
 
 async function renderPage(page, directory) {
+    await page.setViewport({...MAX_VIEWPORT, deviceScaleFactor: program.scale});
+
     const uri = fileUrl(path.join(directory, program.index));
     console.log(`Loading ${uri}`);
-    const status = await page.open(uri);
-    assert(status == 'success');
-    await page.property('viewportSize', MAX_VIEWPORT);
+    await page.goto(uri);
 
     await sleep(program.wait * 1000);
 
     const templateVars = Object.assign({folder: path.basename(directory)}, _.pick(program, ['quality', 'index', 'el', 'wait']));
     const outputFilename = path.normalize(path.join(directory, outputTemplate(templateVars)));
     const outputPath = path.dirname(outputFilename);
-    const format = (path.extname(outputFilename).toLowerCase() == '.png') ? 'png' : 'jpeg';
+    const type = (path.extname(outputFilename).toLowerCase() === '.png') ? 'png' : 'jpeg';
     shell.mkdir('-p', outputPath);
 
-    const bounds = await page.evaluate(function (selector) {
-        return document.querySelector(selector).getBoundingClientRect();
-    }, program.el);
-    await page.property('clipRect', bounds);
-    await page.render(outputFilename, {format, quality: '100'});
+    const clip = await page.evaluate(selector => document.querySelector(selector).getBoundingClientRect().toJSON(), program.el);
+
+    const screenshotParams = {path: outputFilename, clip, type};
+    if (type === 'jpeg') {
+        screenshotParams.quality = 100;
+    }
+    await page.screenshot(screenshotParams);
 
     return outputFilename;
 }
@@ -53,20 +54,20 @@ async function optimizeImages(uncompressedFiles) {
     const targetSize = program.targetsize * 1024;
     assert(targetSize > MIN_FILESIZE, `Provided target size of ${targetSize} bytes is impossibly small`);
 
-    var targetQuality = program.quality;
+    let targetQuality = program.quality;
     assert(targetQuality >= 0 && targetQuality <= 100, `Initial quality should be between 0 and 100, recommend 100`);
 
     const destination = path.join(path.dirname(uncompressedFiles[0]), 'optimized');
-    var needRecompression = _.clone(uncompressedFiles);
+    let needRecompression = _.clone(uncompressedFiles);
 
-    for (var i = 0; needRecompression.length > 0 && i < MAX_TRIES; i++) {
+    for (let i = 0; needRecompression.length > 0 && i < MAX_TRIES; i++) {
         console.log(`Compressing ${needRecompression.length} image[s] at quality ${targetQuality.toFixed(0)}...`);
         const out = await imagemin(needRecompression, {
             destination,
             glob: false,
             plugins: [
                 imagemin_mozjpeg({quality: targetQuality}),
-                imagemin_pngquant({strip: true, quality: [0, targetQuality/100.0]}),
+                imagemin_pngquant({strip: true, quality: [0, targetQuality / 100.0]}),
             ]
         });
         needRecompression = out
@@ -75,25 +76,24 @@ async function optimizeImages(uncompressedFiles) {
         targetQuality *= 0.95;
     }
     if (needRecompression.length > 0) {
-        console.error(`${needRecompression.length} files could not hit target filesize ${(targetSize/1024).toFixed(0)}kb in ${MAX_TRIES} iterations. Try a larger target filesize.`)
+        console.error(`${needRecompression.length} files could not hit target filesize ${(targetSize / 1024).toFixed(0)}kb in ${MAX_TRIES} iterations. Try a larger target filesize.`)
     }
 }
 
 async function go(rootDir) {
-    console.log("Booting up phantomjs...");
-    const instance = await phantom.create([], {phantomPath});
+    console.log("Booting up puppeteer...");
+    const instance = await puppeteer.launch();
     const uncompressedFiles = [];
     try {
-        const page = await instance.createPage();
+        const page = await instance.newPage();
         console.log("Capturing pages...");
         for (const fn of shell.find(rootDir).filter(fn => path.basename(fn) === program.index)) {
             const outputFilename = await renderPage(page, path.dirname(fn));
             console.log(outputFilename);
             uncompressedFiles.push(outputFilename);
         }
-    }
-    finally {
-        await instance.exit();
+    } finally {
+        await instance.close();
     }
 
     if (uncompressedFiles.length > 0) {
@@ -115,6 +115,7 @@ program
         'variables in this string such as {folder} for the folder name, {index} {el} and {wait} from above options. ' +
         'Supported file types are .png and .jpg.', '../backup_{folder}.jpg')
     .option('-w, --wait <sec>', 'Time to wait in seconds', parseFloat, 0.1)
+    .option('-s, --scale <float>', 'Physical pixel scale, use 2 for @2x retina', parseFloat, 1)
     .option('-t, --targetsize <kb>', 'Output to an image with file size equal to the target size in kb', 40)
     .option('-q, --quality <0-100>', 'Compressed image quality for the first pass of compression', parseFloat, 100)
     .parse(process.argv);
